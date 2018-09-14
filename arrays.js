8701d13d28dc83a7e03ca588831ee9ab336eda4a
Cliff Jurgensen <jivefox.002@gmail.com>
2018-09-14 19:05:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array
 }
-function accssElementInArray(array, index) {
+function accessElementInArray(array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
